074a7fbf0bab66d4eab1a804bfe3dfd31cec766b
Jose <joseirigoyenp@gmail.com>
2026-01-21 18:10:50 +0000

    Landing Jose2

--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { Login } from '../pages/auth/Login';
 import { Register } from '../pages/auth/Register';
 import { DatasetsList } from '../pages/datasets/DatasetsList';
@@ -102,6 +102,7 @@ export const AppRoutes = () => {
         />
         
         <Route path="/" element={<Landing />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
       </Routes>
     </PollingProvider>
     </BrowserRouter>
